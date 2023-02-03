d9b0b4969888a2688a532c5d201880440e392c55
emhng <contactemilyhuang@gmail.com>
2023-02-03 08:40:33 -0800

    Add base files for better and support pages

---
+++ src/app/pages/better/better.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-better',
+  templateUrl: './better.component.html',
+  styleUrls: ['./better.component.css']
+})
+export class BetterComponent {
+
+}
---
+++ src/app/pages/better/better.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BetterComponent } from './better.component';
+
+
+
+@NgModule({
+  declarations: [
+    BetterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BetterModule { }
