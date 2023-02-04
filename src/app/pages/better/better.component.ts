5e868391d517588449cb82973508ef74aa43d545
emhng <contactemilyhuang@gmail.com>
2023-02-04 12:12:18 -0800

    Add confetti animation

--- src/app/pages/better/better.component.ts
+++ src/app/pages/better/better.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-better',
-  templateUrl: './better.component.html',
-  styleUrls: ['./better.component.css']
+	selector: 'app-better',
+	templateUrl: './better.component.html',
+	styleUrls: ['./better.component.css']
 })
 export class BetterComponent {
-
+	emojis = [
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🎉',
+		'🥳',
+		'🥳',
+		'🎉'
+	];
 }
